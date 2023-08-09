e49ea45df98dab7e3a56f1d72284839c1e81aa66
Amine derbal <aminedoran31@gmail.com>
2023-08-09 14:12:48 +0100

    Update styling

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,11 @@ export default {
       colors: {
         primary: '#2c2c34',
         secondary: '#aaa6c3',
-        tertiary: '#151030',
+        tertiary: '#2c2c9c',
         'black-100': '#100d25',
         'black-200': '#090325',
         'white-100': '#f3f3f3',
+        nav: '#072448',
       },
       boxShadow: {
         card: '0px 35px 120px -15px #211e35',
