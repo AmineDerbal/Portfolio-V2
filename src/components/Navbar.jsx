1d926f8243974d18df2bc8441d5fc5e8cb7ab1b1
Amine derbal <aminedoran31@gmail.com>
2023-08-09 17:09:38 +0100

    Update navbar background color

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
   return (
     <nav
       className={`${styles.paddingX} w-full flex
- items-center py-5 fixed top-0 z-20 bg-primary`}
+ items-center py-5 fixed top-0 z-20 bg-nav`}
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link
