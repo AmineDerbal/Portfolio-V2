ff8cd3af1133e9b3bf60123cdc6d226de4b13b50
Amine derbal <aminedoran31@gmail.com>
2023-11-21 15:58:01 +0100

    Delete button

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -32,32 +32,20 @@ const About = () => {
       </motion.div>
       <motion.p
         variants={fadeIn('', '', 0.1, 1)}
-        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
       >
-        Full stack developer with experience in building and maintaining web applications, I am
-        adept at working remotely and have the necessary communication and collaboration skills to
-        excel in a virtual work environment.
+        Full stack developer with experience in building and maintaining web
+        applications, I am adept at working remotely and have the necessary
+        communication and collaboration skills to excel in a virtual work
+        environment.
       </motion.p>
-      <motion.div variants={fadeIn('', '', 0.1, 1)} className='mt-4'>
-        <button
-          className='bg-blue-500 hover:bg-blue-400 text-white font-bold py-2 px-4 border-b-4 border-blue-700 hover:border-blue-500 rounded inline-flex items-center'
-          onMouseOver={changeBackground}
-          onMouseOut={resetBackground}
-          onClick={handleDownload}
-        >
-          <svg
-            className='fill-current w-4 h-4 mr-2'
-            xmlns='http://www.w3.org/2000/svg'
-            viewBox='0 0 20 20'
-          >
-            <path d='M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z' />
-          </svg>
-          <span>Get my resume</span>
-        </button>
-      </motion.div>
-      <div className='mt-20 flex flex-wrap w-full justify-center gap-10'>
+      <div className="mt-20 flex flex-wrap w-full justify-center gap-10">
         {services.map((service, index) => (
-          <ServiceCard key={service.title} index={index} {...service} />
+          <ServiceCard
+            key={service.title}
+            index={index}
+            {...service}
+          />
         ))}
       </div>
     </>
