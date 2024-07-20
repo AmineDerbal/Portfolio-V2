24811bbd4402cb03718181b68e5ce0538c7c7276
Amine derbal <aminedoran31@gmail.com>
2024-07-20 21:31:27 +0100

    fix works bu in mobile view

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -7,12 +7,15 @@ const SectionWrapper = (Component, idName) => {
     return (
       <motion.section
         variants={staggerContainer()}
-        initial='hidden'
-        whileInView='show'
-        viewport={{ once: true, amount: 0.25 }}
+        initial="hidden"
+        whileInView="show"
+        viewport={{ once: true, amount: 0.1 }}
         className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
       >
-        <span className='hash-span' id={idName}>
+        <span
+          className="hash-span"
+          id={idName}
+        >
           &nbsp;
         </span>
         <Component />
