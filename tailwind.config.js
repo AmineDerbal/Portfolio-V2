5f832a855fc600a6e22d543b7ef20d8db2461128
Amine derbal <aminedoran31@gmail.com>
2024-07-21 13:55:23 +0100

    Update color

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
       colors: {
         primary: '#2c2c34',
         secondary: '#aaa6c3',
-        tertiary: '#2c2c9c',
+        tertiary: '#100d25',
         'black-100': '#100d25',
         'black-200': '#090325',
         'white-100': '#f3f3f3',
