d1a72cfcc0c4211eb5eeea05e50ff51ea9707fe3
Amine derbal <aminedoran31@gmail.com>
2024-07-21 13:55:34 +0100

    Delete star canvas

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
 import { BrowserRouter } from 'react-router-dom';
-import { Navbar, StarsCanvas, Hero, Works, About, Contact } from './components';
+import { Navbar, Hero, Works, About, Contact } from './components';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div className='relative z-0 bg-primary'>
-        <StarsCanvas />
+      <div className="relative z-0 bg-primary">
         <div>
           <Navbar />
           <Hero />
         </div>
         <Works />
         <About />
-        <div className='relative z-0'>
+        <div className="relative z-0">
           <Contact />
         </div>
       </div>
