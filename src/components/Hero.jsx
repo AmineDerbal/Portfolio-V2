ec26508305cd8b3c640dfa8d632fe3c64a1d3155
Amine derbal <aminedoran31@gmail.com>
2025-07-11 18:50:15 +0100

    Fix animation issue

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
 import { useState } from 'react';
 import PropTypes from 'prop-types';
-import Typed from 'react-typed';
+import { TypeAnimation } from 'react-type-animation';
 import { styles } from '../styles';
 import { socialLinks } from '../constants';
 
@@ -9,8 +9,7 @@ const SocialLink = ({ name, url, icon }) => {
   return (
     <div
       onClick={() => window.open(url, '_blank')}
-      className=" w-10 h-10 rounded-full flex justify-center items-center cursor-pointer-center
-    items-center cursor-pointer"
+      className="w-10 h-10 rounded-full flex justify-center items-center cursor-pointer"
     >
       <img
         src={icon}
@@ -28,70 +27,73 @@ const Hero = () => {
   const [forthTypingComplete, setForthTypingComplete] = useState(false);
 
   return (
-    <section
-      className="relative w-full h-screen
-    mx-auto"
-    >
+    <section className="relative w-full h-screen mx-auto">
       <div
-        className={`${styles.paddingX} absolute inset-0
-      top-[120px] max-w-5xl mx-auto flex flex-row
-      items-start gap-5`}
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-5xl mx-auto flex flex-row items-start gap-5`}
       >
-        <div
-          className="flex flex-col justify-center items-center
-        mt-5"
-        >
+        <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#38ef7d]" />
           <div className="w-1 sm:h-80 h-40 green-gradient" />
         </div>
         <div>
           <h1 className={`${styles.heroHeadText}`}>
-            <Typed
-              strings={['Hi There, I&apos;m&nbsp;']}
-              typeSpeed={40}
-              showCursor={false}
-              onComplete={() => setFirstTypingComplete(true)}
+            <TypeAnimation
+              sequence={["Hi There, I'm ", () => setFirstTypingComplete(true)]}
+              speed={40}
+              cursor={false}
+              wrapper="span"
+              repeat={0}
             />
             {firstTypingComplete && (
-              <span className="text-[#38ef7d]">
-                <Typed
-                  strings={['Amine']}
-                  typeSpeed={40}
-                  showCursor={false}
-                  onComplete={() => setSecondTypingComplete(true)}
-                />
-              </span>
+              <TypeAnimation
+                sequence={[
+                  '',
+                  500,
+                  'Amine',
+                  () => setSecondTypingComplete(true),
+                ]}
+                speed={40}
+                cursor={false}
+                wrapper="span"
+                className="text-[#38ef7d]"
+              />
             )}
           </h1>
 
           {secondTypingComplete && (
-            <p
-              className={`${styles.heroSubText} mt-2
-          text-white-100`}
-            >
-              <Typed
-                strings={['I am a Software developer']}
-                typeSpeed={40}
-                showCursor={false}
-                onComplete={() => setThirdTypingComplete(true)}
+            <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+              <TypeAnimation
+                sequence={[
+                  '',
+                  500,
+                  'I am a Software developer',
+                  () => setThirdTypingComplete(true),
+                ]}
+                speed={40}
+                cursor={false}
+                wrapper="span"
               />
             </p>
           )}
 
           {thirdTypingComplete && (
             <p className="mt-3 sub-text text-secondary text-[17px] max-w-2xl leading-[30px]">
-              <Typed
-                strings={[
-                  'I can help you build a product , feature or website Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hestiate to contact me.',
+              <TypeAnimation
+                sequence={[
+                  '',
+                  500,
+                  'I can help you build a product, feature or website. Look through some of my work and experience! If you like what you see and have a project you need coded, don’t hesitate to contact me.',
+                  () => setForthTypingComplete(true),
                 ]}
-                typeSpeed={40}
-                showCursor={false}
-                onComplete={() => setForthTypingComplete(true)}
+                speed={40}
+                cursor={false}
+                wrapper="span"
               />
             </p>
           )}
+
           {forthTypingComplete && (
-            <div className=" mt-10  flex gap-5 items-center justify-center">
+            <div className="mt-10 flex gap-5 items-center justify-center">
               {socialLinks.map((link) => (
                 <SocialLink
                   key={link.id}
@@ -103,27 +105,17 @@ const Hero = () => {
         </div>
       </div>
 
-      <div
-        className="absolute xs:bottom-8
-      bottom-14 w-full flex justify-center items-center"
-      >
+      <div className="absolute xs:bottom-8 bottom-14 w-full flex justify-center items-center">
         <a href="#work">
-          <div
-            className="w-[35px] h-[64px]
-          rounded-3xl border-4 border-secondary
-          flex justify-center items-start p-2 mt-20"
-          >
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2 mt-20">
             <motion.div
-              animate={{
-                y: [0, 24, 0],
-              }}
+              animate={{ y: [0, 24, 0] }}
               transition={{
                 duration: 1.5,
                 repeat: Infinity,
                 repeatType: 'loop',
               }}
-              className="w-3 h-3 rounded-full bg-secondary
-              mb-1"
+              className="w-3 h-3 rounded-full bg-secondary mb-1"
             />
           </div>
         </a>
